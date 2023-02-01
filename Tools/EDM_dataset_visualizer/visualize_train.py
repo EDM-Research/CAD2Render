07a26db9f572fcd537c91b1e28934b0ad24a8816
Steven <steven.moonen@uhasselt.be>
2023-02-01 16:07:19 +0100

    Update visualizer tool

--- Tools/EDM_dataset_visualizer/visualize_train.py
+++ Tools/EDM_dataset_visualizer/visualize_train.py
@@ -10,18 +10,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+# Inputs
+data_path = "E:\\Datasets\\PILS\\Barco\\bop"
+scene_id = 0
+DisplayAll = True
+
 # model scale
 model_scale = 100   # we assume a unit of mm
 
-# path to root of the dataset
-data_path = "D:/Projects/PILS/renderings/proximus/bop"
-
-## get the number of scenes
+## Select the scene
 all_scenes = os.listdir(os.path.join(data_path, 'train_PBR'))
-print(all_scenes)
-
-# TODO: we assume only the first scene for development
-scene_path = os.path.join(data_path, 'train_PBR', all_scenes[0])
+scene_path = os.path.join(data_path, 'train_PBR', all_scenes[scene_id])
+print("List of all scene folders: " + str(all_scenes))
+print("Displaying results for folder: " + str(scene_path))
 
 ## get the model information
 try:
@@ -29,49 +30,71 @@ try:
     with open(model_map_file, 'r') as infile:
         model_map = json.load(infile)
 except FileNotFoundError:
-    model_map = {"Model_name":"1","Model_id":1}
-print(model_map)
+    model_map = {"1":"obj_000001"}
+print("List of all models in the scene: " + str(model_map))
 
-## read model PLY and sample FPS
-try:
-    # full name in the model filename
-    mesh_path = os.path.join(data_path, 'models', model_map['Model_name']+'.ply')
-    scene = PlyData.read(open(mesh_path, 'rb'))
-except FileNotFoundError:
-    # Model name type 0000001.ply
-    mesh_path = os.path.join(data_path, 'models', str(model_map['Model_id']).zfill(6)+'.ply')
-    scene = PlyData.read(open(mesh_path, 'rb'))
+def getModelBB(object_id):
 
-num_verts = scene['vertex'].count
-vertices = np.zeros(shape=[num_verts, 3], dtype=np.float32)
-vertices[:, 0] = scene['vertex'].data['x']
-vertices[:, 1] = scene['vertex'].data['y']
-vertices[:, 2] = scene['vertex'].data['z']
+    ## read model PLY and sample FPS
+    try:
+        # full name in the model filename
+        mesh_path = os.path.join(data_path, 'models', model_map[str(object_id)]+'.ply')
+        scene = PlyData.read(open(mesh_path, 'rb'))
+    except FileNotFoundError:
+        # Model name type 0000001.ply
+        mesh_path = os.path.join(data_path, 'models', str(model_map[str(object_id)]).zfill(6)+'.ply')
+        scene = PlyData.read(open(mesh_path, 'rb'))
 
-model = np.asarray(vertices)
-model = model*model_scale
+    num_verts = scene['vertex'].count
+    vertices = np.zeros(shape=[num_verts, 3], dtype=np.float32)
+    vertices[:, 0] = scene['vertex'].data['x']
+    vertices[:, 1] = scene['vertex'].data['y']
+    vertices[:, 2] = scene['vertex'].data['z']
 
-# Get Model Corners:
-min_x, max_x = np.min(model[:, 0]), np.max(model[:, 0])
-min_y, max_y = np.min(model[:, 1]), np.max(model[:, 1])
-min_z, max_z = np.min(model[:, 2]), np.max(model[:, 2])
-corners_3d = np.array([
-    [min_x, min_y, min_z],
-    [min_x, min_y, max_z],
-    [min_x, max_y, min_z],
-    [min_x, max_y, max_z],
-    [max_x, min_y, min_z],
-    [max_x, min_y, max_z],
-    [max_x, max_y, min_z],
-    [max_x, max_y, max_z],
-])
+    model = np.asarray(vertices)
+    model = model*model_scale
 
-center_3d = np.reshape(
-            (np.max(corners_3d, 0) + np.min(corners_3d, 0)) / 2, (1, 3))
-diameter = np.linalg.norm(np.asarray(
-            corners_3d[0])-np.asarray(corners_3d[7]))
+    # Get Model Corners:
+    min_x, max_x = np.min(model[:, 0]), np.max(model[:, 0])
+    min_y, max_y = np.min(model[:, 1]), np.max(model[:, 1])
+    min_z, max_z = np.min(model[:, 2]), np.max(model[:, 2])
+    corners_3d = np.array([
+        [min_x, min_y, min_z],
+        [min_x, min_y, max_z],
+        [min_x, max_y, min_z],
+        [min_x, max_y, max_z],
+        [max_x, min_y, min_z],
+        [max_x, min_y, max_z],
+        [max_x, max_y, min_z],
+        [max_x, max_y, max_z],
+    ])
 
-print(f'Model diameter: {diameter}')
+    #center_3d = np.reshape(
+    #            (np.max(corners_3d, 0) + np.min(corners_3d, 0)) / 2, (1, 3))
+    #diameter = np.linalg.norm(np.asarray(
+    #            corners_3d[0])-np.asarray(corners_3d[7]))
+    #print(f'Model diameter: {diameter}')
+    
+    return corners_3d
+#end def getModelBB
+
+edgeColors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff', '#ffffff', '#000000']
+def displayModel(instance_id, image_id, cam_K, gt_data, ax, color):
+    # get rvec, tvec and object id from GT
+    rvec = np.reshape(gt_data[image_id][instance_id]['cam_R_m2c'], [3, 3])
+    tvec = np.array(gt_data[image_id][instance_id]["cam_t_m2c"])
+    obj_id = np.array(gt_data[image_id][instance_id]["obj_id"])
+    corners_3d = getModelBB(obj_id)
+
+
+    # project corner onto image
+    corners_2d, _ = cv2.projectPoints(corners_3d, rvec, tvec, cam_K, np.array([]))
+    corners_2d = np.squeeze(corners_2d)
+
+    ax.imshow(vis)
+    ax.add_patch(patches.Polygon(xy=corners_2d[[0, 1, 3, 2, 0, 4, 6, 2]], fill=False, linewidth=1, edgecolor=color))
+    ax.add_patch(patches.Polygon(xy=corners_2d[[5, 4, 6, 7, 5, 1, 3, 7]], fill=False, linewidth=1, edgecolor=color))
+#end def displayModel
 
 ## read GT file
 gt_file = os.path.join(scene_path, 'scene_gt.json')
@@ -83,25 +106,23 @@ cam_file = os.path.join(scene_path, 'scene_camera.json')
 with open(cam_file, 'r') as infile:
     cam_data = json.load(infile)
 
-for im, cam in zip(gt_data, cam_data):
-    img_file = os.path.join(scene_path, 'rgb', str(im).zfill(6)+'.jpg')
+for image_id, cam in zip(gt_data, cam_data):
+    img_file = os.path.join(scene_path, 'rgb', str(image_id).zfill(6)+'.jpg')
     vis = cv2.imread(img_file, cv2.COLOR_BGR2RGB)
-
-    # get rvec and tvec from GT
-    rvec = np.reshape(gt_data[im][0]['cam_R_m2c'], [3, 3])
-    tvec = np.array(gt_data[im][0]["cam_t_m2c"])
-
+    
     # get the camera matrix
-    cam_K = np.array(cam_data[im]["cam_K"]).reshape([3, 3]).astype('float')
-
-    # project corner onto image
-    corners_2d, _ = cv2.projectPoints(corners_3d, rvec, tvec, cam_K, np.array([]))
-    corners_2d = np.squeeze(corners_2d)
+    cam_K = np.array(cam_data[image_id]["cam_K"]).reshape([3, 3]).astype('float')
 
     fig, ax = plt.subplots()
-    ax.imshow(vis)
-    ax.add_patch(patches.Polygon(xy=corners_2d[[0, 1, 3, 2, 0, 4, 6, 2]], fill=False, linewidth=1, edgecolor='g'))
-    ax.add_patch(patches.Polygon(xy=corners_2d[[5, 4, 6, 7, 5, 1, 3, 7]], fill=False, linewidth=1, edgecolor='g'))
-    ax.set_title(str(im).zfill(6)+'.jpg')
+    
+    for instance_id in range(len(gt_data[image_id])):
+        edgeColor = "#777777"
+        if(instance_id < len(edgeColors)):
+            edgeColor = edgeColors[instance_id]
+        displayModel(instance_id, image_id, cam_K, gt_data, ax, edgeColor)
+        if(DisplayAll == False):
+            break
+    
+    ax.set_title(str(image_id).zfill(6)+'.jpg')
     plt.show()
     
