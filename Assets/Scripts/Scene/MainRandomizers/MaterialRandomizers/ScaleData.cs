c15affd5b173d762a735a00ce0ce783df08e9d73
Steven-Moonen-Farad <steven.moonen@uhasselt.be>
2026-08-12 14:24:02 +0200

    Move scale randomizer from material to instance randomization

---
+++ Assets/Scripts/Scene/MainRandomizers/MaterialRandomizers/ScaleData.cs
@@ -0,0 +1,21 @@
+﻿
+using SneakySquirrelLabs.MinMaxRangeAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Untitled Dataset", menuName = "Cad2Render/Material randomizer Data/New Scale randomize Data")]
+public class ScaleData: ScriptableObject
+{
+
+    [Tooltip("Keep Aspect Ratio when scaling. Only x scale is used when enabled.")]
+    public bool keepAspectRatio = false;
+
+    [Tooltip("min random scale factor")]
+    public Vector3 minScale = new Vector3(0.5f, 0.5f, 0.5f);
+    [Tooltip("max random scale factor")]
+    public Vector3 maxScale = new Vector3(2.0f, 2.0f, 2.0f);
+}
--- Assets/Scripts/Scene/MainRandomizers/MaterialRandomizers/ScaleHandler.cs
+++ Assets/Scripts/Scene/MainRandomizers/MaterialRandomizers/ScaleHandler.cs
@@ -6,23 +6,54 @@ using UnityEngine;
 public class ScaleHandler : MaterialRandomizerInterface
 {
     Vector3 previousScale;
-    private void Start()
+    private void Awake()
     {
         previousScale = new Vector3(1, 1, 1);
     }
+    public override int getPriority() { return 69; }
 
-    //public RotationData dataset;
-    //[InspectorButton("TriggerCloneClicked")]
-    //public bool clone;
-    //private void TriggerCloneClicked()
-    //{
-    //    RandomizerInterface.CloneDataset(ref dataset);
-    //}
+    public ScaleData dataset;
+    [InspectorButton("TriggerCloneClicked")]
+    public bool clone;
+    private void TriggerCloneClicked()
+    {
+        RandomizerInterface.CloneDataset(ref dataset);
+    }
 
+    public override void RandomizeSingleInstance(GameObject instance, ref RandomNumberGenerator rng)
+    {
+        Vector3 nextScale = new Vector3(
+            rng.Range(dataset.minScale.x, dataset.maxScale.x),
+            rng.Range(dataset.minScale.y, dataset.maxScale.y),
+            rng.Range(dataset.minScale.z, dataset.maxScale.z)
+            );
+        if (dataset.keepAspectRatio)
+        {
+            nextScale.y = nextScale.x;
+            nextScale.z = nextScale.x;
+        }
+        previousScale.x = 1 / previousScale.x;
+        previousScale.y = 1 / previousScale.y;
+        previousScale.z = 1 / previousScale.z;
+        var currentScale = instance.transform.localScale;
+        currentScale.Scale(previousScale);
+        currentScale.Scale(nextScale);
+        instance.transform.localScale = currentScale;
+        previousScale = nextScale;
+    }
 
     public override void RandomizeSingleMaterial(MaterialTextures textures, ref RandomNumberGenerator rng)
     {
-        Vector3 nextScale = new Vector3(rng.Next()*0.3f + 0.85f, rng.Next() * 0.3f + 0.85f, rng.Next() * 0.3f + 0.85f);
+        return;
+        Vector3 nextScale = new Vector3(
+            rng.Range(dataset.minScale.x, dataset.maxScale.x), 
+            rng.Range(dataset.minScale.y, dataset.maxScale.y), 
+            rng.Range(dataset.minScale.z, dataset.maxScale.z)
+            );
+        if(dataset.keepAspectRatio) {
+            nextScale.y = nextScale.x;
+            nextScale.z = nextScale.x;
+        }
         previousScale.x = 1 / previousScale.x;
         previousScale.y = 1 / previousScale.y;
         previousScale.z = 1 / previousScale.z;
